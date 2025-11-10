1827af5c037187c3c28dadc5979ad7b525cac24e
Sandropython <sandro.deise@hotmail.com>
2025-11-10 04:17:27 -0300

    add whitenoise and templates configuration

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from siteapp.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),        # raiz /
-    path("healthz", views.healthz, name="healthz"),
+    path("", home, name="home"),
 ]
